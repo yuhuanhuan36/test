b5cde7fcaa33aed8fb69b0ef86d0399132eac99b
yuhuanhuan <yuhuanhuan@shuidihuzhu.com>
2019-09-05 20:28:14 +0800

    update set 方法

---
+++ yhh/program/ConditionControlTest.py
@@ -0,0 +1,11 @@
+#coding utf-8
+# @Time: 2019/9/5 19:55
+# @Author: yuhuanhuan
+#条件控制练习
+a = 1
+while a <10:
+    if (a % 2 == 0):
+        print('偶数:', a)
+    else:
+        print('基数:', a)
+    a += 1
--- yhh/program/FirstStep.py
+++ yhh/program/FirstStep.py
@@ -1,3 +1,17 @@
 #coding utf-8
 # @Time: 2019/9/5 19:46
 # @Author: yuhuanhuan
+# Fibonacci series: 斐波纳契数列
+a, b = 0, 1
+
+while b < 10:
+    print('b:', b, end=',')
+    #print('a:', a, end=',')
+    a, b = b, a + b
+
+
+i = 256 * 256
+print('i:', i)
+'''
+    关键字end可以用于将结果输出到同一行，或者在输出的末尾添加不同的字符
+'''
